d974a76095eb38f23022fdab07a39fe995285e67
hyayi <98936899+hyayi@users.noreply.github.com>
2022-12-04 23:59:36 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,8 +1,6 @@
 python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name regnet_y_32gf    --batch_size 2 --strategy ddp --device 4
 python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name convnext_base --batch_size 2 --strategy ddp --device 4
-python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name densenet161 --batch_size 4 --strategy ddp --device 4
 python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name efficientnet_b7 --batch_size 4 --strategy ddp --device 4
 python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name efficientnet_v2_l --batch_size 2 --strategy ddp --device 4
-python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name efficientnet_v2_m --batch_size 2 --strategy ddp --device 4
-python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name efficientnet_v2_s --batch_size 2 --strategy ddp --device 4
-python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name resnet101  --batch_size 4 --strategy ddp --device 4
+
+
