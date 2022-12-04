0c3ec965f8741c1e8639ea9153ec9a24ab6b1527
hyayi <98936899+hyayi@users.noreply.github.com>
2022-12-04 22:32:38 +0900

    Update train2.sh

--- train2.sh
+++ train2.sh
@@ -1,1 +1,1 @@
-python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name resnet50    --batch_size 8 --strategy ddp --device 4
+python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name resnet50 --batch_size 16 --strategy ddp --device 1
