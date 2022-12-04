93d3be09845c460c28a0bc9c7a6d4c29dd463f8b
hyayi <98936899+hyayi@users.noreply.github.com>
2022-12-04 21:05:19 +0900

    Update pathmodel.py

--- models/pathmodel.py
+++ models/pathmodel.py
@@ -20,3 +20,32 @@ class MILNetImage(nn.Module):
         aggregated_feature = self.aggregator(patch_features)
         result = self.classifier(aggregated_feature)
         return result
+
+    
+class MILNetImageMLP(nn.Module):
+    """Training with image only"""
+
+    def __init__(self,model_config):
+        super().__init__()
+
+        self.image_feature_extractor = getattr(backbone, model_config['backbone_name'])(**model_config['backbone_params'])
+        self.classifier = nn.Sequential(
+            nn.Linear(self.image_feature_extractor.output_size, 1024),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 2)
+        )
+    def forward(self, bag_data):
+        bag_data = bag_data.squeeze(0)  # [1 (batch size), N, C, H, W] --> [N, C, H, W], remove the batch dimension
+        patch_features = self.image_feature_extractor(bag_data)
+        result = self.classifier(patch_features)
+        return result
