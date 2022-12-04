b9cd939e1441ef835e32b162ce638eaf9fab69ec
hyayi <98936899+hyayi@users.noreply.github.com>
2022-12-04 21:44:26 +0900

    Create train2.sh

---
+++ train2.sh
@@ -0,0 +1,1 @@
+python3 train.py --train_path /home/lab/inseo/cancer/data/train_re.csv --test_path /home/lab/inseo/cancer/data/test_re.csv --weight_path /home/lab/inseo/cancer/ --log_path /home/lab/inseo/cancer/ --model_name resnet50    --batch_size 8 --strategy ddp --device 4
