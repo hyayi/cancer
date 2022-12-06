79019b31a5105cbd726295dcc2147b179e9eca3a
hyayi <98936899+hyayi@users.noreply.github.com>
2022-12-06 17:10:28 +0900

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,4 @@
-from sklearn.preprocessing import LabelEncoder,QuantileTransformer
+from sklearn.preprocessing import LabelEncoder,QuantileTransformer,StandardScaler
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
